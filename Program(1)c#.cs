18b650147c0aaa271b0e777308c23b10c1d96240
ileazyPL <103658395+ileazyPL@users.noreply.github.com>
2022-04-14 20:21:46 +0300

    Add files via upload

---
+++ Program(1)c#.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+string name = "Alex"; 
+Console.WriteLine($"Write your name:{name}");
+Console.WriteLine($"Hello, {name}!");
+
+
---
+++ Program(2)c#.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+while (true)
+{ 
+    Console.Clear();
+    double a, b;
+    string action;
+
+    
+    try
+    {
+
+        Console.WriteLine("Введите операнд 1:");
+        a = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите операнд 2:");
+        b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Оператор: '+' '-' '*' '/' ");
+        action = Console.ReadLine();
+    }
+    catch(Exception)
+    {
+        Console.WriteLine("Операнд должен являться числом !");
+        Console.ReadLine();
+        continue;
+    }
+        switch (action)
+        {
+            case "+":
+                Console.WriteLine(a + b);
+                break;
+            case "-":
+                Console.WriteLine(a - b);
+                break;
+            case "*":
+                Console.WriteLine(a * b);
+                break;
+
+            case "/":
+                if (b == 0)
+                {
+                    Console.WriteLine("Ошибка!");
+                }
+                else
+                {
+                    Console.WriteLine(a / b);
+                }
+                break;
+            
+            default:
+                Console.WriteLine("Ошибка!");
+                break;
+        }
+    Console.ReadLine();
+    
+}
